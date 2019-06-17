84871ea1d83b199d3af317a53d2dc5f21899fe82
Lucas <lucasladeira2001@gmail.com>
2019-06-17 10:39:12 -0300

    Começando Login e Cadastro

---
+++ projeto/conexãoBD().php
@@ -0,0 +1,20 @@
+<?php
+
+connect_BD(){
+
+$conexao = mysqli_connect("host","username","password","dbname","port");
+if (!$conexao)
+die ("Erro de conexão com o servidor, o seguinte erro ocorreu -> ".mysqli_error());
+//conectando com a tabela do banco de dados
+$banco = mysqli_select_db("bdname",$conexao);
+if (!$banco)
+die ("Erro de conexão com banco de dados, o seguinte erro ocorreu -> ".mysqli_error());
+
+return $conexão;
+}
+
+
+
+
+
+?>
