9862c96aa35e0dc3b2d895d15182f283a8452613
Renancastro23 <renancastro123306@gmail.com>
2019-06-24 09:04:30 -0300

    comentando

--- projeto/conexaoBD.php
+++ projeto/conexaoBD.php
@@ -2,15 +2,11 @@
 // Função para se conectar ao banco de dados
 function connect_BD() { 
 
-$conexao = mysqli_connect("localhost","root","","Cadastro","3306");
-if (!$conexao)
-die ("Erro de conexão com o servidor, o seguinte erro ocorreu -> ".mysqli_error($conexao));
-
-return $conexao;
+    $conexao = mysqli_connect("localhost","root","","Cadastro","3306");
+    if (!$conexao)
+    die ("Erro de conexão com o servidor, o seguinte erro ocorreu -> ".mysqli_error($conexao));
+    return $conexao;
 }
-
-
-
 
 
 ?>
--- projeto/login.php
+++ projeto/login.php
@@ -1,13 +1,14 @@
 <?php
-include_once('/conexaoBD.php');
+include_once('/conexaoBD.php'); // Insere o arquivo de conexão ao banco
 
-$banco = connect_BD();
+$banco = connect_BD(); // Conexão banco de dados.
 
+// Coletar informações do formulario
 $matricula = $banco->real_escape_string($_POST["matricula"]);
 $senha = md5($_POST["senha"]);
 
 
-
+// Executar query sqls
 $query_login = "SELECT * FROM login WHERE matricula = ? and senha = ?";
 $stmt = $banco->prepare($query_login);
 $stmt->bind_param("ss", $matricula, $senha);
@@ -15,10 +16,11 @@ $stmt->execute();
 $stmt->store_result();
 $rows = $stmt->num_rows;
 
+// Verificar o resultado
 if ($rows > 0){
-    echo 'Logado com sucesso!';
-    $banco->close();
-} else {
+        echo 'Logado com sucesso!';
+        $banco->close();
+    } else {
     echo 'Falha no login, verifique os dados.';
     $banco->close();
 }
