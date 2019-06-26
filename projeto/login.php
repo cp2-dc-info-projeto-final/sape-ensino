496c2ce7d053d8363ddc19a8e8edb871ef0cc151
Leniad Asid <danynovaisdias@outlook.com>
2019-06-26 16:31:20 -0300

    sesssion start add

--- projeto/login.php
+++ projeto/login.php
@@ -1,6 +1,8 @@
 <?php
 include_once('/conexaoBD.php'); // Insere o arquivo de conexão ao banco
 
+session_start(); // Iniciando a sessão
+
 $banco = connect_BD(); // Conexão banco de dados.
 
 // Coletar informações do formulario
@@ -8,23 +10,25 @@ $matricula = $banco->real_escape_string($_POST["matricula"]);
 $senha = md5($_POST["senha"]);
 
 
-// Executar query sqls
-$query_login = "SELECT * FROM login WHERE matricula = ? and senha = ?";
+// Executar query sql
+$query_login = "SELECT nome, matricula, senha, cargo FROM login WHERE matricula = ? and senha = ?";
 $stmt = $banco->prepare($query_login);
 $stmt->bind_param("ss", $matricula, $senha);
 $stmt->execute();
 $stmt->store_result();
-$rows = $stmt->num_rows;
-
+$stmt->bind_result($nome, $matricula, $senha, $cargo);
+$stmt->fetch();
 // Verificar o resultado
-if ($rows > 0){
-        echo 'Logado com sucesso!';
-        $banco->close();
+if ($stmt->num_rows > 0){
+    echo 'Logado com sucesso!';
+    $_SESSION['nome'] = $nome;
+    $_SESSION['cargo'] = $cargo;
+    $banco->close();
     } else {
     echo 'Falha no login, verifique os dados.';
     $banco->close();
 }
 
-
+$stmt->close();
 
 ?>
