b24de353ff25ceb0ce7f81043c929aa07baadee7
kah750 <kah750@gmail.com>
2019-07-10 17:18:36 -0300

    add pagina de turmas para docente

--- projeto/funcphp/mostrarescolas.php
+++ projeto/funcphp/mostrarescolas.php
@@ -10,12 +10,17 @@
     
     while($row = $result->fetch_assoc()){
     echo '<div class="card col-3 m-4">';
-            echo '<div class="card-body">
-						<h3 class="card-tite"  style="white-space:nowrap;">'.$row['enome'].'</h3>
-						<h5 class="card-subtitle text-muted mb-2">'.$row['lnome'].'</h5>
-						<p class="card-text text-muted text-justify">'.$row['descricao'].'</p>
-						<a class="text-decoration-none" href=""><button class="btn btn-primary btn-block bt-2">Entrar</button></a>
-                </div>'; 
+            echo '
+                    <div class="card-header">
+                        <h3 class="card-tite text-center mb-3">'.$row['enome'].'</h3>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-subtitle text-muted mb-2">'.$row['lnome'].'</h5>
+                        <p class="card-text text-muted text-justify">'.$row['descricao'].'</p>
+                    </div>
+                    <div class="card-footer">
+						<a class="text-decoration-none position-relative-bottom" href=""><button class="btn btn-primary btn-block bt-2">Entrar</button></a>
+                    </div>'; 
     echo '</div>';
     }
    
