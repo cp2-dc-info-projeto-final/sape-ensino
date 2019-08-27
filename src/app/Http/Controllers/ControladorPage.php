103c138bcae95d1c3648a31ca7553748dec401e1
LeinadAsid <danynovaisdias@outlook.com>
2019-08-27 19:24:39 -0300

    tirando foreach sem necessidade

--- src/app/Http/Controllers/ControladorPage.php
+++ src/app/Http/Controllers/ControladorPage.php
@@ -18,13 +18,10 @@ class ControladorPage extends Controller
     }
 
     public function escolas(){
-        $escolas = Escolas::all();
-        foreach($escolas as $es){
-            $diretor = DB::table('escolas')
-            ->select('escolas.nome', 'escolas.descricao', 'users.name')
-            ->join('users', 'users.id', '=', 'escolas.diretor')
-            ->get('name', 'diretor');
-        }
+        $diretor = DB::table('escolas')
+        ->select('escolas.nome', 'escolas.descricao', 'users.name')
+        ->join('users', 'users.id', '=', 'escolas.diretor')
+        ->get('name', 'diretor');
         return view('Paginas.escolas')->with(array('diretor' => $diretor));
     }
 
